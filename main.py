d1251c03a78dd739de93429d86f982947199ff79
sultan aljrboa <sultanaljrboua@gmail.com>
2023-07-10 21:23:38 +0300

    upload

--- main.py
+++ main.py
@@ -332,3 +332,92 @@
 # elif computer_choice == user_choice:
 #     print("It's a draw")
 
+             #{Calculates the average student height}#
+
+# student_heights = input("Input a list of student heights ").split()
+# for n in range(0, len(student_heights)):
+#   student_heights[n] = int(student_heights[n])
+# the_av = len(student_heights)
+# the_total = sum(student_heights)
+# div = (the_total / the_av)
+# print(round(div))
+
+                 #{Calculates the highest score}#
+
+# student_scores = input("Input a list of student scores ").split()
+# for n in range(0, len(student_scores)):
+#   student_scores[n] = int(student_scores[n])
+#
+# the_total = max(student_scores)
+# print(f"The highest score in the class is: {the_total}")
+
+                 #{FizzBuzz Game!}#
+
+# for number in range(1, 101):
+#     if  number % 3 ==0 and number % 5 == 0:
+#         print("FizzBuzz")
+#     elif number % 3 ==0:
+#         print("Fizz")
+#     elif number % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(number)
+
+
+
+
+                    #{PyPassword Generator}#
+
+# import random                                     #an in-built module of Python that is used to generate random numbers in Python
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+# numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+#
+# print("Welcome to the PyPassword Generator!")
+# nr_letters= int(input("How many letters would you like in your password?\n"))
+# nr_symbols = int(input(f"How many symbols would you like?\n"))
+# nr_numbers = int(input(f"How many numbers would you like?\n"))
+#
+#                         #Eazy Pass
+#
+# # password =""
+# #
+# # for char in range(1, nr_letters + 1):
+# #     password += random.choice(letters)
+# #
+# # for char in range(1, nr_symbols + 1):
+# #     password += random.choice(symbols)
+# #
+# # for char in range(1, nr_numbers + 1):
+# #     password += random.choice(numbers)
+# #
+# # print(password)
+#
+#                      #Hard Pass
+#
+# password_list =[]                                  #all value in the list
+#
+# for char in range(1, nr_letters + 1):              #the range of the input user betwen (1 + input user + 1)
+#     password_list.append(random.choice(letters))   #a pre-defined method used to add a single item to certain collection types
+#
+# for char in range(1, nr_symbols + 1):              #etc ^
+#     password_list.append(random.choice(symbols))   #etc..
+#
+# for char in range(1, nr_numbers + 1):              #etc ^
+#     password_list.append(random.choice(numbers))   #etc..
+#
+# random.shuffle(password_list)                      #for mex the number and symbols and letters
+#
+# password =""                                       #for git a password in the same line
+# for char in password_list:
+#     password += char                               #put the (char in the password)
+#
+# print(f"Your password is: {password}")             #print the password with (f) for add the mex variuble
+
+          #{How to create Function}#
+
+def my_function():    #first (def) and name of function():
+print("Hello")
+#print("Bye")
+
+my_function()
